22f52af3f7746f72bd7a3beb6b85653fd629b055
ali-wetrill <ali.ahmed@berkeley.edu>
2022-05-04 15:44:39 +0000

    added tings

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -6,14 +6,48 @@ set -eu pipefail
 ##### Brev.dev setup script. See docs for more: https://docs.brev.dev          #####
 ####################################################################################
 
+(echo ""; echo "##### PWD #####"; echo "";)
+echo ""
+pwd
+echo ""
+
 ##### Node v14.x + npm #####
 (echo ""; echo "##### Node v14.x + npm #####"; echo "";)
+
 sudo apt install ca-certificates
+
+(echo ""; echo "##### PWD #####"; echo "";)
+echo ""
+pwd
+echo ""
+
+
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+(echo ""; echo "##### PWD #####"; echo "";)
+echo ""
+pwd
+echo ""
+
+
 sudo apt-get install -y nodejs
 
+(echo ""; echo "##### PWD #####"; echo "";)
+echo ""
 pwd
+echo ""
 
 (echo ""; echo "##### Setup Deps #####"; echo "";)
 npm install --also=dev
-npm run build 
+
+(echo ""; echo "##### PWD #####"; echo "";)
+echo ""
+pwd
+echo ""
+
+npm run build
+
+(echo ""; echo "##### PWD #####"; echo "";)
+echo ""
+pwd
+echo ""
